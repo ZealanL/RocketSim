75cb21196174fc9da57d3ca85bab74d75943636f
ZealanL <zlaferte@gmail.com>
2022-08-06 10:54:38 -0700

    Start class CarConfig (WIP)

---
+++ src/CarConfig/CarConfig.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "../BaseInc.h"
+
+struct WheelPair {
+	// Radius of both wheels
+	float wheelRadius;
+
+	// How far the wheels can be pulled away before they stop moving
+	float maxSuspensionExtendDist;
+
+	// How much force do these wheels push with
+	float drivePushForce;
+
+	// How fast do they spin
+	float spinRate;
+};
+
+struct CarConfig {
+	// Area of the hitbox (relative to car origin)
+	Vec hitboxMin, hitboxMax;
+
+	// Front and back wheel pairs
+	WheelPair wheelPairs[2]; 
+};
