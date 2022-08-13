8a8faf11fdc98f8cd5b7028cde9dd7b72422c0d1
ZealanL <zlaferte@gmail.com>
2022-08-12 21:40:18 -0700

    Add CarState, implement state getting/setting with it

--- src/Sim/Car/Car.cpp
+++ src/Sim/Car/Car.cpp
@@ -1,1 +1,36 @@
 #include "Car.h"
+
+// Update our internal state from bullet and return it
+CarState Car::GetState() {
+
+	btRigidBody* rb = bulletVehicle.getRigidBody();
+	btTransform rbTransform = rb->getWorldTransform();
+
+	internalState.pos = rbTransform.getOrigin() * BT_TO_UU;
+
+	rbTransform.getRotation().getEulerZYX(internalState.angles.yaw, internalState.angles.pitch, internalState.angles.roll);
+	
+	internalState.vel = rb->getLinearVelocity();
+
+	internalState.angVel = rb->getAngularVelocity();
+
+	return internalState;
+}
+
+// Update our bullet stuff to this new state, replace our internal state with it
+void Car::SetState(const CarState& state) { 
+	btRigidBody* rb = bulletVehicle.getRigidBody();
+	btTransform rbTransform = rb->getWorldTransform();
+
+	rbTransform.setOrigin(state.pos * UU_TO_BT);
+
+	btQuaternion quat;
+	quat.setEulerZYX(state.angles.yaw, state.angles.pitch, state.angles.roll);
+	rbTransform.setRotation(quat);
+
+	rb->setLinearVelocity(state.vel);
+
+	rb->setAngularVelocity(state.angVel);
+
+	this->internalState = state;
+}
--- src/Sim/Car/Car.h
+++ src/Sim/Car/Car.h
@@ -1,5 +1,51 @@
 #pragma once
 #include "CarConfig/CarConfig.h"
+#include "../btVehicleRL/btVehicleRL.h"
+#include "../CarControls.h"
+
+struct CarState {
+	// Position in world space
+	Vec pos;
+
+	EulerAngle angles;
+
+	// Linear velocity
+	Vec vel;
+
+	// Angular velocity (axis-angle)
+	Vec angVel;
+
+	bool isOnGround;
+	bool hasJumped, hasDoubleJumped;
+
+	// Goes from 0 to 100
+	float boost;
+
+	// This is a state variable due to the supersonic maintain time (see RLConst.h)
+	bool isSupersonic;
+
+	// The most recent controls this car was simulated with
+	// Required for jumping/flipping, the last value of the jump button is required, because jumping occurs on press only
+	CarControls controls;
+};
+
+enum class Team {
+	BLUE = 0,
+	ORANGE = 1
+};
+
 class Car {
+public:
 	CarConfig config;
+	Team team;
+
+	CarState GetState();
+	void SetState(const CarState& state);
+
+protected:
+	btVehicleRL bulletVehicle;
+	
+	// NOTE: Not all values are updated because they are unneeded for internal simulation
+	// Those values are only updated when GetState() is called
+	CarState internalState;
 };
