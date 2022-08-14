cd9833528d48a59dc9606c997133a5d97e197a34
ZealanL <zlaferte@gmail.com>
2022-08-13 21:17:21 -0700

    Add linear piecewise curve class LinearPieceCurve

--- src/Math/Math.cpp
+++ src/Math/Math.cpp
@@ -1,1 +1,30 @@
 #include "Math.h"
+
+float LinearPieceCurve::GetOutput(float input) const {
+	float output = input;
+
+	if (!valueMappings.empty()) {
+
+		// Make sure it isnt before/at the first value mapping
+		auto& firstValPair = *valueMappings.begin();
+		if (input <= firstValPair.first)
+			return firstValPair.second;
+
+		for (auto itr = std::next(valueMappings.begin()); itr != valueMappings.end(); itr++) {
+			if (itr->first > input) {
+				// Found the point bigger than it! Get surrounding
+				auto& afterPair = *itr;
+				auto& beforePair = *std::prev(itr);
+
+				float rangeBetween = afterPair.first - beforePair.first;
+				float valDiffBetween = afterPair.second - beforePair.second;
+				float linearInterpFactor = (input - beforePair.first) / rangeBetween;
+				return beforePair.second + valDiffBetween * linearInterpFactor;
+			}
+		}
+
+		// Must be beyond the largest input mapping, return that
+		return std::prev(valueMappings.end())->second;
+	}
+	return output;
+}
--- src/Math/Math.h
+++ src/Math/Math.h
@@ -1,6 +1,12 @@
 #pragma once
 #include "../BaseInc.h"
 
+struct LinearPieceCurve {
+	map<float, float> valueMappings;
+
+	float GetOutput(float input) const;
+};
+
 namespace Math {
 	
 }
