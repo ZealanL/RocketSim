4e23917de67f64e64fde38fac5fc3825af31407a
ZealanL <zlaferte@gmail.com>
2023-01-23 15:26:48 -0800

    Fix CarControls struct initialization

--- src/Sim/CarControls.h
+++ src/Sim/CarControls.h
@@ -15,14 +15,17 @@ struct CarControls {
 	// Maybe someday...
 	// bool useItem;
 
-	CarControls() = default;
+	CarControls() {
+		// Initialize everything as zero
+		memset(this, 0, sizeof(CarControls));
+	}
 
 	// Makes all values range-valid (clamps from -1 to 1)
 	void ClampFix() {
-		throttle	= CLAMP(throttle,	-1, 1);
-		steer		= CLAMP(steer,		-1, 1);
-		pitch		= CLAMP(pitch,		-1, 1);
-		yaw			= CLAMP(yaw,		-1, 1);
-		roll		= CLAMP(roll,		-1, 1);
+		throttle	= RS_CLAMP(throttle,	-1, 1);
+		steer		= RS_CLAMP(steer,		-1, 1);
+		pitch		= RS_CLAMP(pitch,		-1, 1);
+		yaw			= RS_CLAMP(yaw,		-1, 1);
+		roll		= RS_CLAMP(roll,		-1, 1);
 	}
 };
