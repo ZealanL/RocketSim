d10594c9164c8adb65367783b570d792d1eb5bd3
ZealanL <zlaferte@gmail.com>
2023-02-16 01:00:52 -0800

    Switch out .vcxproj for CMakeLists.txt

---
+++ src/RocketSim.h
@@ -0,0 +1,8 @@
+// MASTER HEADER FILE
+// Includes everything a user of RocketSim probably wants
+
+#include "Sim/Car/Car.h"
+#include "Sim/Ball/ball.h"
+#include "Sim/Arena/Arena.h"
+
+#include "Sim/Math/Math.h"
