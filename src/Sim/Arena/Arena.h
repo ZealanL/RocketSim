0e4cdb8cb03be101cdce3ae3f22859447cc01b9b
Eric Veilleux <virx@virxcase.dev>
2023-02-28 18:07:26 -0500

    Update Arena.h

--- src/Sim/Arena/Arena.h
+++ src/Sim/Arena/Arena.h
@@ -62,7 +62,7 @@ public:
 	// Simulate everything in the arena for a given number of ticks
 	RSAPI void Step(int ticksToSimulate = 1);
 
-	RSAPI void ResetToKickoff(int seed = -1);
+	RSAPI void ResetToRandomKickoff(int seed = -1);
 
 	// Free all associated memory
 	RSAPI ~Arena();
