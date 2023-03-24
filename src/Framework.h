d2ff9f4c2c0617e0801948b13e8af399ba4d2d8f
ZealanL <zlaferte@gmail.com>
2023-03-23 18:04:57 -0700

    Remove unused integer typedefs from Framework.h

--- src/Framework.h
+++ src/Framework.h
@@ -46,11 +46,6 @@ using std::string;
 using std::wstring;
 using std::pair;
 
-// Integer typedefs
-typedef int8_t	int8;	typedef uint8_t	 uint8;
-typedef int16_t int16;	typedef uint16_t uint16;
-typedef int32_t int32;	typedef uint32_t uint32;
-typedef int64_t int64;	typedef uint64_t uint64;
 typedef uint8_t byte;
 
 // Current millisecond time
