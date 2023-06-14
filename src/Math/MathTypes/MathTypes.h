4ca39eee32e0a0995a0d25ec03594e533112905f
ZealanL <zlaferte@gmail.com>
2023-06-14 13:31:43 -0700

    Fix Vec.Cross() using wrong subtraction order

--- src/Math/MathTypes/MathTypes.h
+++ src/Math/MathTypes/MathTypes.h
@@ -41,7 +41,7 @@ struct RS_ALIGN_16 Vec {
 	Vec Cross(const Vec& other) const {
 		return Vec(
 			 (y * other.z) - (z * other.y),
-			-(x * other.z) - (z * other.x),
+			 (z * other.x) - (x * other.z),
 			 (x * other.y) - (y * other.x)
 		);
 	}
