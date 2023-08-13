d0c231e2cccba74235c167d738e6ed018b36b6e7
ZealanL <zlaferte@gmail.com>
2023-08-12 19:32:13 -0700

    Don't dynamically-allocate std::runtime_error in RS_ERR_CLOSE macro

--- src/Framework.h
+++ src/Framework.h
@@ -58,7 +58,7 @@ typedef uint8_t byte;
 
 #define RS_WARN(s) RS_LOG("WARNING: " << s)
 
-#define RS_ERR_CLOSE(s) { std::string _errorStr = RS_STR("FATAL ERROR: " << s); RS_LOG(_errorStr); throw new std::runtime_error(_errorStr); exit(EXIT_FAILURE); }
+#define RS_ERR_CLOSE(s) { std::string _errorStr = RS_STR("FATAL ERROR: " << s); RS_LOG(_errorStr); throw std::runtime_error(_errorStr); exit(EXIT_FAILURE); }
 
 #if 0 // FOR FUTURE USE: Exports/imports setup
 #ifdef ROCKETSIM_EXPORTS
