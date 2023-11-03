627fd77ff493608f2dd774bb3ac6fec83996d892
ZealanL <zlaferte@gmail.com>
2023-11-02 19:41:44 -0700

    Fix goal scoreback calling for wrong team (thanks mtheall)

--- src/Sim/Arena/Arena.cpp
+++ src/Sim/Arena/Arena.cpp
@@ -739,7 +739,7 @@ void Arena::Step(int ticksToSimulate) {
 
 		if (_goalScoreCallback.func != NULL) { // Potentially fire goal score callback
 			if (IsBallScored()) {
-				_goalScoreCallback.func(this, RS_TEAM_FROM_Y(ball->_rigidBody.m_worldTransform.m_origin.y()), _goalScoreCallback.userInfo);
+				_goalScoreCallback.func(this, RS_TEAM_FROM_Y(-ball->_rigidBody.m_worldTransform.m_origin.y()), _goalScoreCallback.userInfo);
 			}
 		}
 
