db0bb8b7e684b48425e9fe5a4c39a1d56b8c8172
ZealanL <zlaferte@gmail.com>
2023-12-08 21:07:01 -0800

    Add comment labels for hitbox type in CarConfig.cpp

--- src/Sim/Car/CarConfig/CarConfig.cpp
+++ src/Sim/Car/CarConfig/CarConfig.cpp
@@ -16,12 +16,12 @@
 //    the ones everyone else uses/shares do not.
 
 const static Vec HITBOX_SIZES[6] = { 
-	{ 120.507f,	86.6994f,	38.6591f	},
-	{ 130.427f,	85.7799f,	33.8f		},
-	{ 131.32f,	87.1704f,	31.8944f	},
-	{ 133.992f,	83.021f,	32.8f		},
-	{ 129.519f,	84.6879f,	36.6591f	},
-	{ 123.22f,	79.2103f,	44.1591f	}
+	{ 120.507f,	86.6994f,	38.6591f	}, // OCTANE
+	{ 130.427f,	85.7799f,	33.8f		}, // DOMINUS
+	{ 131.32f,	87.1704f,	31.8944f	}, // PLANK
+	{ 133.992f,	83.021f,	32.8f		}, // BREAKOUT
+	{ 129.519f,	84.6879f,	36.6591f	}, // HYBRID
+	{ 123.22f,	79.2103f,	44.1591f	}  // MERC
 };
 
 const static Vec HITBOX_OFFSETS[6] = { 
