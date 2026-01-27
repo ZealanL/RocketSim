673e907e69f9c07f0dd01d9810ad1efcc9bd5bdb
ZealanL <36944229+ZealanL@users.noreply.github.com>
2026-01-27 14:26:00 -0800

    Add public `glam_inc.rs` file in src root

---
+++ rocketsim/src/glam_inc.rs
@@ -0,0 +1,4 @@
+pub use glam::{BVec2, BVec3, BVec4};
+pub use glam::{IVec2, IVec3, IVec4};
+pub use glam::{Mat2, Mat3, Mat3A, Mat4};
+pub use glam::{Vec2, Vec2Swizzles, Vec3, Vec3A, Vec3Swizzles, Vec4};
--- rocketsim/src/lib.rs
+++ rocketsim/src/lib.rs
@@ -7,8 +7,9 @@ pub mod shared;
 mod sim;
 #[cfg(feature = "vis")]
 pub mod vis;
-
+mod glam_inc;
 ///////////
 
 pub use crate::sim::*;
 pub use base::*;
+pub use glam_inc::*;
