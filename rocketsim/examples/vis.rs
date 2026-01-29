5bd14b197de0f638d0e5919413d4f12d18f838a2
ZealanL <36944229+ZealanL@users.noreply.github.com>
2026-01-29 02:54:01 -0800

    Add more vis example keybinds

--- rocketsim/examples/vis.rs
+++ rocketsim/examples/vis.rs
@@ -1,4 +1,6 @@
 use device_query::{DeviceQuery, DeviceState, Keycode};
+use egui::Key;
+use glam::Vec3A;
 use rocketsim::{
     Arena, ArenaConfig, CarBodyConfig, CarControls, GameMode, Team, init_from_default,
 };
@@ -59,13 +61,35 @@ fn main() {
     arena.set_vis_enabled(true);
 
     let device_state = DeviceState::new();
+    let mut prev_keys = Vec::new();
     loop {
-        let keys = device_state.get_keys();
+        let held_keys = device_state.get_keys();
+
+        let mut pressed_keys: Vec<Keycode> = held_keys
+            .iter()
+            .cloned()
+            .filter(|key| !prev_keys.contains(key))
+            .collect();
+
         let controls = determine_controls(&device_state);
 
         // Reset arena
-        if keys.contains(&Keycode::Backspace) || arena.tick_count() == 0 {
+        if pressed_keys.contains(&Keycode::Backspace) || arena.tick_count() == 0 {
             arena.reset_to_random_kickoff();
+        }
+
+        if pressed_keys.contains(&Keycode::Key2) {
+            // Teleport ball to dribble position
+            let car_state = arena.get_car_state(car_idx);
+            let mut ball_state = *arena.get_ball_state();
+            ball_state.phys.pos = car_state.phys.pos + Vec3A::new(0.0, 0.0, 150.0);
+            ball_state.phys.vel = car_state.phys.vel;
+            arena.set_ball_state(ball_state);
+        } else if pressed_keys.contains(&Keycode::Key4) {
+            // Launch ball
+            let mut ball_state = *arena.get_ball_state();
+            ball_state.phys.vel += Vec3A::new(0.0, 0.0, 1000.0);
+            arena.set_ball_state(ball_state);
         }
 
         arena.set_car_controls(car_idx, controls);
@@ -73,5 +97,7 @@ fn main() {
         arena.step_tick();
 
         std::thread::sleep(std::time::Duration::from_millis(8));
+
+        prev_keys = held_keys;
     }
 }
