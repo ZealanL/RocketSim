b0225b8154025458b6dc13aa0f8a29770e4c358c
Virx <virx@virxcase.dev>
2026-04-24 21:14:22 -0400

    Optimize `VehicleRL` wheel construction

--- rocketsim/src/bullet/dynamics/vehicle/wheel_info.rs
+++ rocketsim/src/bullet/dynamics/vehicle/wheel_info.rs
@@ -1,15 +1,5 @@
 use glam::Vec3A;
 
-#[derive(Clone, Copy)]
-pub struct WheelInfoConstructionInfo {
-    pub chassis_connection_cs: Vec3A,
-    pub wheel_direction_cs: Vec3A,
-    pub wheel_axle_cs: Vec3A,
-    pub suspension_rest_length: f32,
-    pub wheel_radius: f32,
-}
-
-#[derive(Default)]
 pub struct RaycastInfo {
     pub contact_normal_ws: Vec3A,
     pub contact_point_ws: Vec3A,
@@ -21,12 +11,23 @@ pub struct RaycastInfo {
     pub ground_obj: Option<usize>,
 }
 
+impl RaycastInfo {
+    const DEFAULT: Self = Self {
+        contact_normal_ws: Vec3A::ZERO,
+        contact_point_ws: Vec3A::ZERO,
+        suspension_length: 0.0,
+        hard_point_ws: Vec3A::ZERO,
+        wheel_direction_ws: Vec3A::ZERO,
+        wheel_axle_ws: Vec3A::ZERO,
+        is_in_contact: false,
+        ground_obj: None,
+    };
+}
+
 pub struct WheelInfo {
     pub raycast_info: RaycastInfo,
     pub axle_dir: Vec3A,
     pub chassis_connection_point_cs: Vec3A,
-    pub wheel_direction_cs: Vec3A,
-    pub wheel_axle_cs: Vec3A,
     pub suspension_rest_length_1: f32,
     pub wheels_radius: f32,
     pub engine_force: f32,
@@ -37,20 +38,16 @@ pub struct WheelInfo {
 }
 
 impl WheelInfo {
-    pub fn new(ci: WheelInfoConstructionInfo) -> Self {
-        Self {
-            suspension_rest_length_1: ci.suspension_rest_length,
-            wheels_radius: ci.wheel_radius,
-            chassis_connection_point_cs: ci.chassis_connection_cs,
-            wheel_direction_cs: ci.wheel_direction_cs,
-            wheel_axle_cs: ci.wheel_axle_cs,
-            engine_force: 0.0,
-            brake: 0.0,
-            raycast_info: RaycastInfo::default(),
-            axle_dir: Vec3A::ZERO,
-            clipped_inv_contact_dot_suspension: 0.0,
-            suspension_relative_vel: 0.0,
-            wheels_suspension_force: 0.0,
-        }
-    }
+    pub const DEFAULT: Self = Self {
+        chassis_connection_point_cs: Vec3A::ZERO,
+        suspension_rest_length_1: 0.0,
+        wheels_radius: 0.0,
+        engine_force: 0.0,
+        brake: 0.0,
+        raycast_info: RaycastInfo::DEFAULT,
+        axle_dir: Vec3A::ZERO,
+        clipped_inv_contact_dot_suspension: 0.0,
+        suspension_relative_vel: 0.0,
+        wheels_suspension_force: 0.0,
+    };
 }
