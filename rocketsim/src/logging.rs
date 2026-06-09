564258f36b4582c1f2f7698856925dfed558f4cd
swz-git <contact@swz.works>
2026-06-09 19:08:25 +0200

    replace env_logger with fern

--- rocketsim/src/logging.rs
+++ rocketsim/src/logging.rs
@@ -1,12 +1,11 @@
-use std::io::Write;
-
-use env_logger::WriteStyle;
 use log::LevelFilter;
 
 pub fn try_init() -> Result<(), log::SetLoggerError> {
-    env_logger::builder()
-        .format(|buf, record| writeln!(buf, "[RSIM | {}] {}", record.level(), record.args()))
-        .write_style(WriteStyle::Always)
-        .filter(None, LevelFilter::Info)
-        .try_init()
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!("[RSIM | {}] {}", record.level(), message));
+        })
+        .level(LevelFilter::Info)
+        .chain(std::io::stderr())
+        .apply()
 }
